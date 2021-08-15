77f7a5c04e6b90ee3b1ae8c7e3d7e9a502690aba
jacek <mendyk.ja@gmail.com>
2021-08-15 10:30:30 +0200

    updating 'learning_python.py' file

--- learning_python.py
+++ learning_python.py
@@ -1,3 +1,19 @@
+# read input file
+file_object = open("learning_python.txt", "rt")
+# read file contents to string
+data = file_object.read()
+# replace all occurrences of the required string
+data = data.replace('C', 'Python')
+# close the input file
+file_object.close()
+# open the input file in write mode
+file_object = open("learning_python.txt", "wt")
+# overwrite the input file with the resulting data
+file_object.write(data)
+# close the file
+print(data)
+file_object.close()
+
 file_name = 'learning_python.txt'
 
 with open(file_name) as file_object:
