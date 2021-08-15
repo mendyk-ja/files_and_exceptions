b66bcbe54fdb04aac206c32f61ae322c623a25f4
jacek <mendyk.ja@gmail.com>
2021-08-15 16:24:31 +0200

    addin g 'division_calculator.py file

---
+++ division_calculator.py
@@ -0,0 +1,16 @@
+print('Enter two numbers, which will be divided.')
+print("Enter 'q' to exit program.")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print(answer)
