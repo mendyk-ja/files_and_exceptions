e4a6305a1cadd0539663567d1bf5301b843c172f
jacek <mendyk.ja@gmail.com>
2021-09-01 22:31:00 +0200

    Refactoring 'remember_me.py' file

--- remember_me.py
+++ remember_me.py
@@ -1,8 +1,37 @@
 import json
 
-username = input("What is your name? ")
 
-filename= 'username.json'
-with open(filename, 'w') as f:
-    json.dump(username, f)
-    print(f"Your name i saved now and will be used later, {username}.")
+def get_stored_username():
+    """Extracting a name from file, if  it exists."""
+    filename = 'username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def  get_new_username():
+    """Asking user to give his/her name and then saving it to a file."""
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+
+def greet_user():
+    """Greeting user using his name."""
+    username = get_stored_username()
+    if username:
+        print(f"Welcome again, {username}!")
+    else:
+        username = get_new_username()
+        print(f"Your name i saved now and will be used later, {username}.")
+
+
+greet_user()
+
+
